2608a9888796698e61521d3334a97545aae0fe69
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2024-09-02 07:17:16 -0300

    feat: :sparkles: manipulate amount input

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
-console.log("JavaScript carregado!");
+const amount = document.getElementById("amount");
+
+// Manipulando o input amount para receber somente números
+amount.addEventListener("input", () => {
+  const characters = /\D+/g;
+  amount.value = amount.value.replace(characters, "");
+});
